b30351213008c3bb0be631c51904f9a638682c2e
User <mdshishov@mail.ru>
2024-12-12 04:24:42 +0300

    Промежуточное сохранение (5)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  showModal();
   const contactTypes = {
     phone: 'Телефон',
     email: 'Email',
@@ -7,13 +8,65 @@ document.addEventListener('DOMContentLoaded', () => {
     other: 'Другое',
   }
 
-  const form = document.getElementById('form');
-  form.querySelectorAll('.form__name-input').forEach((input) => {
-    input.addEventListener('keypress', noSpaces);
-    input.addEventListener('blur', controlInputLable);
-  })
+  // const form = document.getElementById('form');
+  // form.querySelectorAll('.form__name-input').forEach((input) => {
+  //   input.addEventListener('keypress', noSpaces);
+  //   input.addEventListener('blur', controlInputLable);
+  // })
 
-  console.log(form.elements.lastName);
+  // const select = document.querySelector('.form__contacts__select__toggle');
+  
+  // document.addEventListener('click', (event) => {
+  //   const target = event.target;
+  //   if (target.closest('[data-select]') === null) {
+  //     const form = document.getElementById('form');
+  //     if (form) {
+  //       form.querySelectorAll('[data-arrow-icon].arrow-icon_up').forEach((icon) => {
+  //         icon.classList.remove('arrow-icon_up')
+  //       });
+  //       form.querySelectorAll('[data-select-options]').forEach((selectOptions) => {
+  //         selectOptions.classList.add('closed');
+  //       });
+  //     }
+  //   }
+  // })
+
+  // select.addEventListener('click', () => {
+  //   select.parentNode.querySelector('[data-arrow-icon]').classList.toggle('arrow-icon_up');
+  //   select.parentNode.querySelector('[data-select-options]').classList.toggle('closed');
+  // })
+});
+
+function showModal(data = null) {
+  const modal = document.createElement('div');
+  modal.id = 'modal';
+  modal.classList.add('modal');
+
+  const form = document.createElement('form');
+  form.id = 'form';
+  form.classList.add('form');
+  modal.append(form);
+
+  const closeButton = document.createElement('button');
+  closeButton.type = 'button';
+  closeButton.classList.add('form__close-button');
+  closeButton.addEventListener('click', () => {
+    //Функция закрытия модалки
+  })
+  form.append(closeButton);
+
+  const nameInputsField = document.createElement('fieldset');
+  nameInputsField.name = 'name-inputs';
+  nameInputsField.classList.add('form__name-inputs');
+  form.append(nameInputsField);
+
+  
+
+  document.body.append(modal);
+
+  function closeForm(event) {
+    
+  } 
 
   //Отменяет ввод пробелов в текстовом поле
   function noSpaces(event) {
@@ -41,5 +94,18 @@ document.addEventListener('DOMContentLoaded', () => {
       label.classList.remove('form__name-label_small');
     }
   }
-});
+}
+
+//Показывает красную строку с ошибкой
+function showError(message = null) {
+  let aside = document.getElementById('aside');
+  if (!aside) {
+    aside = document.createElement('aside');
+    aside.classList.add('error');
+    aside.id = 'error';
+  }
+  aside.textContent = message
+    ? 'Упс... Кажется, что-то пошло нет так. Пожалуйста, перезагрузите страницу.'
+    : message;
+}
 
